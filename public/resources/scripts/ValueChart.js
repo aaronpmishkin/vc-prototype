55b9bc04bf3a7a0ddb1a2a30d4a0561be7bd6787
aaronpmishkin <aaronpmishkin@gmail.com>
2016-05-09 16:57:50 -0700

    Replace functions I wrote in the ValueChart web prototype with their equivelents built into to d3.
    - Remove manual calculation of stacked bar chart offsets. These are now found using the built in layout.stack() function.
    - Change scaling of data to be accomplished through the built in scale.linear() function.
    - Change structure of data so that it is a mutlidimensional array, rather than a array of objects. Arrays are the natural data format for d3; this makes operations significantly easier.

--- public/resources/scripts/ValueChart.js
+++ public/resources/scripts/ValueChart.js
@@ -1,15 +1,15 @@
 /*
 * @Author: aaronmishkin
 * @Date:   2016-05-05 14:14:04
-* @Last Modified by:   aaronmishkin
-* @Last Modified time: 2016-05-06 09:48:53
+* @Last Modified by:   aaronpmishkin
+* @Last Modified time: 2016-05-09 16:56:06
 */
 
 function ValueChart(dataLocation) {
 
 	this.dataLocation = dataLocation;
 	this.data = [];
-	this.colors = {};
+	this.alternativeLabels = [];
 
 	this.graphSpace = {};
 	this.stackedBarChart = {};
@@ -19,119 +19,138 @@ function ValueChart(dataLocation) {
 
 	var _this = this;
 
-	d3.csv(this.dataLocation, function(error, rows) {
-		_this.data = rows;
-		_this.initChartColors();
+	d3.csv(this.dataLocation, function(d) {
+		if (!_this.objectiveLabels) {
+			_this.objectiveLabels = d3.keys(d);
+			_this.objectiveLabels.splice(0, 1);
+		}
+
+		return d3.values(d);
+		
+	}, function(error, rows) {
+
+		_this.data = rows[0].map(function(col, colIndex) { 
+			return rows.map(function(row, rowIndex) { 
+				if (colIndex == 0) {
+					_this.alternativeLabels.push(row[colIndex]);
+				}
+				return {x: rowIndex, y: +row[colIndex]}; 
+			})
+		});
+
+
+		_this.data.splice(0, 1);
+		_this.layers = _this.data.length;
+		_this.elements = _this.data[0].length;
+
+		_this.color = d3.scale.linear()
+    		.domain([0, _this.elements - 1])
+    		.range(['#00F0FF', '#FF0000']);
+
 		_this.plotChart();
 	});
 }
 
 ValueChart.prototype.configuration = {
 	chartHeight: 1000,
-	chartWidth: 1000,
-	barXOffset: 150,
+	chartWidth: 400,
 	barWidth: 20,
-	alternativeSpacing: 50,
-	textYOffset: 300,
-	textXOffset: 60
+	objectiveSpacing: 80,
+	textXOffset: 10
 }
 
-ValueChart.prototype.plotChart = function() {
 
+ValueChart.prototype.plotChart = function() {
 	var _this = this;
 
 	this.graphSpace = d3.select('#chart')
-		.attr("width", this.configuration.chartWidth)
-		.attr("height", this.configuration.chartHeight);
+		.attr('width', this.configuration.chartWidth)
+		.attr('height', this.configuration.chartHeight);
 
-	this.stackedBarChart = this.graphSpace.append('g');
-
-	this.stackedBars = this.stackedBarChart.selectAll('g')
-		.data(this.data)
-		.enter().append("g")
-			.attr("transform", function(d, i) {return "translate(" + ((_this.configuration.barWidth * i) + _this.configuration.barXOffset) + ",100)";});
-
+	this.stackedBarChart = this.graphSpace.append('g')
+		.attr('transform', 'translate(' + (this.configuration.chartWidth / 5) + ', 100)');
 
 	this.utilityCharts = this.graphSpace.append('g')
 		
 	this.utilityBars = this.utilityCharts.selectAll('g')
 		.data(this.data)
-		.enter().append("g")
-			.attr("transform", function(d, i) {return "translate(" + ((_this.configuration.barWidth * i) + _this.configuration.barXOffset) + ", 300)";});
+		.enter().append('g')
+			.attr('transform', function(d, i) { 
+				return 'translate(' + (_this.configuration.chartWidth / 5) + ',' + ((_this.configuration.objectiveSpacing * i) + (_this.configuration.objectiveSpacing * 5)) + ')'; 
+			})
+			.style('fill', function(d, i) { return _this.color(i); });
 
 
-	this.plotIndividualAlternatives();
 	this.plotStackBarChart();
-}
-
-ValueChart.prototype.initChartColors = function() {
-	this.colors = Object.assign({}, this.data[0]);
-	for (field in this.colors) {
-		this.colors[field] = this.getRandomColor();
-	}
+	this.plotIndividualAlternatives();
 }
 
 ValueChart.prototype.plotIndividualAlternatives = function() {
 	var _this = this;
 
-	var i = this.data.length - 1;
-	for (var field in this.data[0]) {
+	var yMax = d3.max(this.data, function(layer) { return d3.max(layer, function(d) { return d.y; }); });
 
-		if (field !== "alternative") {
-			this.utilityBars.append("rect")
-				.attr("height", function(d) { return d[field]; })
-				.attr("width", this.configuration.barWidth - 1)
-				.attr("y", function(d) { return (i * _this.configuration.alternativeSpacing) - d[field];})
-				.attr("fill", function(d) {return _this.colors[field]});
+	var yScale = d3.scale.linear()
+    	.domain([0, yMax])
+    	.range([0, yMax * 3]);
 
-			this.utilityCharts.append("text")
-				.text(function(d) { return field; })
-				.attr("x", this.configuration.textXOffset)
-				.attr("y", function(d) { return (i * _this.configuration.alternativeSpacing) + _this.configuration.textYOffset;})
-				.attr("fill", "black");
-		}
-	i--;
-	}
+	this.utilityBars.selectAll('rect')
+		.data(function(d) { return d; })
+		.enter().append('rect')
+			.attr('height', function(d) { return yScale(d.y); })
+			.attr('width', this.configuration.barWidth - 1)
+			.attr('y', function(d) { return _this.configuration.objectiveSpacing - yScale(d.y); })
+			.attr('x', function(d, i) { return (i * _this.configuration.barWidth); });
+
+	this.utilityCharts.selectAll('text')
+		.data(this.objectiveLabels)
+		.enter().append('text')
+			.text(function(d) { return d; })
+			.attr('x', this.configuration.textXOffset)
+			.attr('y', function(d, i) { return ((i + 1) * _this.configuration.objectiveSpacing) + (_this.configuration.objectiveSpacing * 5);})
+			.attr('fill', 'black');
+
 }
 
 ValueChart.prototype.plotStackBarChart = function() {
 	var _this = this;
 
-	var columnBaseHeight = {};
+	var stack = d3.layout.stack();
+	
+	layersData = stack(this.data);
 
-	for (var i = this.data.length - 1; i >= 0; i--) {
-		columnBaseHeight[this.data[i].alternative] = this.configuration.barXOffset;
-	}
+	var yMax = d3.max(layersData, function(layer) { return d3.max(layer, function(d) { return d.y + d.y0; }); });
 
-	for (var field in this.data[0]) {
+    var yScale = d3.scale.linear()
+    	.domain([0, yMax])
+    	.range([yMax * 3, 0]);
 
-		if (field !== "alternative") {
 
-			this.stackedBars.append("rect")
-			    .attr("height", function(d) { return (d[field]); })
-				.attr("width", this.configuration.barWidth - 1)
-				.attr("y", function(d) {
-					columnBaseHeight[d.alternative] = columnBaseHeight[d.alternative] - d[field];
-					return columnBaseHeight[d.alternative]; 
-				})
-				.attr("fill", function(d) {return _this.colors[field]});
-		} else {
-			this.stackedBars.append("text")
-				.text(function(d) { return d[field]; })
-				.attr("transform", "rotate(-90)")
-				.attr("y", function(d,i) { return (i) + _this.configuration.barWidth / 2; })
-				.attr("x", -30)
-				.attr("fill", "black");
-		}
-	}
+    this.stackedBars = this.stackedBarChart.selectAll('g')
+		.data(layersData)
+		.enter().append('g')
+		.style('fill', function(d, i) { return _this.color(i); });
 
+	this.stackedBarChart.selectAll('text')
+		.data(this.alternativeLabels)
+		.enter().append('text')
+			.text(function(d) { return d; })
+			.attr('transform', "rotate(-90)")
+			.attr('x', this.configuration.textXOffset)
+			.attr('y', function(d, i) { return (i * _this.configuration.barWidth) + 15})
+			.attr('fill', 'black');
+
+	this.stackedBars.selectAll('rect')
+		.data(function(d) {
+		 return d; })
+		.enter()
+		.append('rect')
+			.attr('x', function(d) { return d.x * _this.configuration.barWidth; })
+    		.attr('y', function(d) { return yScale(d.y0 + d.y); })
+    		.attr('width', this.configuration.barWidth - 1)
+    		.attr('height', function(d) { return yScale(d.y0) - yScale(d.y0 + d.y);});
 }
 
-ValueChart.prototype.getRandomColor = function() {
-    var letters = '0123456789ABCDEF'.split('');
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
+
+
+
